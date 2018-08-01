5ee10bc38b8f889f369b7d0f054e661ff935855d
Zagan202 <lawrenceterralawson@gmail.com>
2018-08-01 23:59:33 +0000

    10.1 completed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180719205855) do
+ActiveRecord::Schema.define(version: 20180801212946) do
 
   create_table "users", force: :cascade do |t|
     t.string "name"
